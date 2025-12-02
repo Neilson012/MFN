abb147ad735b4466e4542abae1019bf473247655
Neilson012 <150353173+Neilson012@users.noreply.github.com>
2025-12-02 11:26:17 -0300

    Update index.js

--- bdd/index.js
+++ bdd/index.js
@@ -1,1 +1,14 @@
+const { Sequelize } = require("sequelize")
+require("dotenv").config()
 
+const Name = process.env.DB_NAME;
+const User = process.env.DB_USER;
+const Host = process.env.DB_HOST;
+const Password = process.env.DB_PASSWORD;
+
+const sequelize = new Sequelize(Name, User, Password, {
+    dialect: "mysql",
+    host: Host,
+});
+
+module.exports =  sequelize;
