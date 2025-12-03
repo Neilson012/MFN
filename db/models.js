5f01f1a131c58491ad5d538b6c0d54fc6fe653a8
Neilson012 <150353173+Neilson012@users.noreply.github.com>
2025-12-03 18:18:46 -0300

    Define Sequelize models for Tecnico, Presidente, Time, and Jogador

--- db/models.js
+++ db/models.js
@@ -1,1 +1,96 @@
+const { Sequelize } = require("sequelize")
+const db = require("./index")
 
+const Tecnico = db.define('Tecnico', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    senha: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+}, { tableName: 'tecnicos' })
+
+const Presidente = db.define('Presidente', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    senha: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+}, { tableName: 'presidentes' })
+
+const Time = db.define('Time', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    endereco: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    tecnico: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    telefone: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+}, { tableName: 'times' })
+
+const Jogador = db.define('Jogador', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    numero_camisa: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    cpf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+    },
+}, { tableName: 'jogadores' })
+
+module.exports = { Tecnico, Presidente, Time, Jogador }
