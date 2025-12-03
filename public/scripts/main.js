712b6d8df60ecc3c7acdd1f70dd34c193a7d8c42
Neilson012 <150353173+Neilson012@users.noreply.github.com>
2025-12-03 19:25:52 -0300

    Implement navbar height adjustment and alert feature
    Add responsive navbar height adjustment and alert display

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,37 @@
+const navbar = document.querySelector("nav")
 
+function setFullSize() {
+    let container = document.querySelector(".h-screen")
+    if (!navbar) {
+        container.style.height = "100vh"
+        return
+    };
+    container.style.height = `calc(100vh - ${navbar.offsetHeight}px)`
+}
+
+setFullSize()
+
+window.addEventListener('resize', () => {
+    setFullSize()
+});
+
+// alert
+
+const urlParams = new URLSearchParams(window.location.search);
+
+const msg = urlParams.get("msg")
+const msgStatus = urlParams.get("status") || "error"
+
+if(msg) {
+    const alert = document.createElement("div");
+    
+    alert.classList.add("alert", "enter", msgStatus);
+    alert.textContent = msg;
+
+    document.body.appendChild(alert);
+
+    setTimeout(() => {
+        alert.classList.remove("enter");
+        alert.classList.add("leave");
+    }, 2000);
+}
