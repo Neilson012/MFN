0b93505cb58846efd27e138a31f187595b7a436a
Neilson012 <150353173+Neilson012@users.noreply.github.com>
2025-12-03 19:26:51 -0300

    Add search functionality to filter elements
    Implement search filtering for elements based on input value.

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,1 +1,34 @@
+const input = document.querySelector(".search")
+const elements = document.querySelectorAll(".time, .jogadores")
+const register = document.querySelector("[class^='register-']")
+const registerHeight = register.offsetHeight
 
+console.log(registerHeight)
+function filterElement(el) {
+    const name = el.querySelector("h3").textContent
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase();
+    const query = input.value
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase();
+
+    return !name.includes(query)
+}
+
+input.addEventListener("input", () => {
+    elements.forEach(el => { el.style.display = "flex" })
+
+    if(input.value == "") return;
+    const result = Array.from(elements).filter(filterElement)
+    
+    result.forEach(el => { el.style.display = "none" })
+    if(result.length == elements.length) register.style.height = `${registerHeight}px`
+})
+
+/* gambiarra */
+
+if (elements.length == 0) {
+    register.style.height = "150px"
+}
