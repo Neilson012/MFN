dad2eb081534bd5076b842266050773dfcadb3c4
chicoinz <franciscomoraisdeoliveira4@gmail.com>
2025-12-06 20:36:17 -0300

    Update accountController.js

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { Membro } = require("../db/models");
+const { Membro } = require("../bdd/models");
 const bcrypt = require("bcrypt");
 
 function renderLogin(_, res) {
