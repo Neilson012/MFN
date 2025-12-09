dd6f8ac5317767a6e0f37d4ffac3788a47faa614
chicoinz <franciscomoraisdeoliveira4@gmail.com>
2025-12-08 23:37:06 -0300

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,9 @@
 const jogadorController = require("./jogadorController")
 const tecnicoController = require("./tecnicoController")
 const timeController = require("./timeController")
-const presidenteController = require("./presidenteController")
 
 module.exports = { 
     jogadorController, 
     tecnicoController, 
     timeController, 
-    presidenteController 
 }
