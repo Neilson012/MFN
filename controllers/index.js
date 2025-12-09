e7bbd3a7976b35b8e8731af0af67b45abb94f126
fumasacomputaria <nicolasjogosklm@gmail.com>
2025-12-08 23:47:03 -0300

    Delete controllers directory

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const jogadorController = require("./jogadorController")
-const tecnicoController = require("./tecnicoController")
-const timeController = require("./timeController")
-
-module.exports = { 
-    jogadorController, 
-    tecnicoController, 
-    timeController, 
-}
